96d7b1572043e924e39c57f1766923afc75fcb9d
Oviya Malarvannan <oviyamalarvannan0305@gmail.com>
2026-06-25 19:14:04 +0530

    Add Financial Forecasting exercise to Module 1

---
+++ Module 1- Algorithms Data structures/FinancialForecasting/src/FinancialForecast.java
@@ -0,0 +1,37 @@
+public class FinancialForecast {
+
+    // Recursive method to calculate future value
+    public static double predictFutureValue(
+            double currentValue,
+            double growthRate,
+            int years) {
+
+        // Base Case
+        if (years == 0) {
+            return currentValue;
+        }
+
+        // Recursive Call
+        return predictFutureValue(
+                currentValue * (1 + growthRate),
+                growthRate,
+                years - 1);
+    }
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10; // 10%
+        int years = 5;
+
+        double futureValue =
+                predictFutureValue(
+                        currentValue,
+                        growthRate,
+                        years);
+
+        System.out.println("Future Value after "
+                + years + " years = "
+                + futureValue);
+    }
+}
