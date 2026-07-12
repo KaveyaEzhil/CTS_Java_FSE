12fa824efba7be131cabd7cc3340984f6ee2c4f4
KaveyaEzhil <kaveyae7@gmail.com>
2026-07-12 15:04:32 +0530

    Rename Main to LibraryManagementApplication to match exercise 2 specs

--- Module 5 - Spring Framework/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
+++ Module 5 - Spring Framework/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.library.service.BookService;
 
-public class Main {
+public class LibraryManagementApplication {
     public static void main(String[] args) {
         // Load the Spring context
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
@@ -12,7 +12,7 @@ public class Main {
         // Retrieve the BookService bean
         BookService bookService = (BookService) context.getBean("bookService");
 
-        // Call the service method
+        // Call the service method to verify setter injection
         bookService.listBooks();
     }
 }
